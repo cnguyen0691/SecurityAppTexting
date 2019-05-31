d3b6a6e38fe872ad3746ff0a294e7aa4f8f1f054
cnguyen0691 <chaunguyen0691@gmail.com>
2019-05-31 13:37:39 -0400

    initial

---
+++ src/main/java/com/example/textmessageappwithsecurity/TextmessageappwithsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.textmessageappwithsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextmessageappwithsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextmessageappwithsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/textmessageappwithsecurity/TextmessageappwithsecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.textmessageappwithsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TextmessageappwithsecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
